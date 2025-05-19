eb551935a8685a5bb9f0c2fc946b95717af8dd04
mirko-console-dir <cdmirko.saponaro@gmail.com>
2025-05-19 18:00:35 +0900

    create basic routes, Layouts and relative main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./protected/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./protected/auth/Welcome";
+import NotesList from "./protected/notes/NotesList";
+import UsersList from "./protected/users/UsersList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {" "}
+      // this parent route will wrap all routes
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          {/* Here all protected routes  */}
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/* End Dash */}
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom"; // render the children of Outlet component
+
+function Layout() {
+  return <Outlet />;
+}
+
+export default Layout;
